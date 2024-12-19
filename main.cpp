acd8719a096977667e975a369f23d06a78b54a5e
Lev <misterh2003@gmail..com>
2024-12-19 16:55:37 +0700

    add main.cpp

---
+++ main.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+
+double my_pow(double chislo, unsigned int stepen)
+{
+    double result = chislo;
+    for (unsigned int i; i < stepen; i++)
+        result *= chislo;
+
+    return result;
+}
+
+int main()
+{
+
+}
